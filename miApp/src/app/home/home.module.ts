301a8a45094ad9ca796c9cb62daabc69a6f6c628
cristina <cristina.ruiz@iesdoctorbalmis.com>
2025-03-03 14:16:44 +0100

    Agrega el componente HeroSection y su integración en la página de inicio

--- miApp/src/app/home/home.module.ts
+++ miApp/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HeroSectionComponent } from '../components/hero-section/hero-section.component';
 
 
 @NgModule({
@@ -12,8 +13,11 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    HeroSectionComponent
+  ]
 })
 export class HomePageModule {}
